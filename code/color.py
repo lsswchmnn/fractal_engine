b00b63723f1151e931dc9d4e8603c16ba2cf606e
lsswchmnn <lasse.wiechmann@web.de>
2026-04-25 15:40:47 +0200

    Postprocessing aus color.py entfernt

--- code/color.py
+++ code/color.py
@@ -562,27 +562,3 @@ class Colorizer():
                 progress_callback(y1, height)
 
         return image
-
-#------------------------------------------------------------
-# POSTPROCESSING für Farbgebungsergebnisse
-
-    def apply_gamma(self, image: np.ndarray, gamma: float = 2.2) -> np.ndarray:
-        if gamma <= 0:
-            return image
-
-        img = image.astype(np.float64) / 255.0
-        img = np.power(img, 1.0 / gamma)
-        img = np.clip(img * 255.0, 0, 255).astype(np.uint8)
-        return img
-
-    def apply_contrast(self, image: np.ndarray, contrast: float = 1.2) -> np.ndarray:
-        img = image.astype(np.float64) / 255.0
-        img = (img - 0.5) * contrast + 0.5
-        img = np.clip(img, 0.0, 1.0)
-        return (img * 255).astype(np.uint8)
-
-    def apply_inversion(self, image: np.ndarray) -> np.ndarray:
-        img = image.astype(np.float32) / 255.0
-        img = 1.0 - img
-        img = (img * 255).astype(np.uint8)
-        return img

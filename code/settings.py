fd28f00766eaee3ffebaf5c7bc2aaf302f0ba807
lsswchmnn <lasse.wiechmann@web.de>
2026-05-01 18:13:42 +0200

    Defaulteinstellungen angepasst

--- code/settings.py
+++ code/settings.py
@@ -6,7 +6,7 @@ class RenderSettings:
     iterate_factor_k        : int   = 250       # Feintuning-Faktor für quantitative Verbesserung der Detailgenauigkeit bei starken Zooms
     export_factor           : int   = 4         # Faktor für die Hochskalierung bei Export
     tile_height             : int   = 32        # Höhe der Kacheln für das tile-basierte Rendering (Performance-Optimierung)
-    supersampling_enabled   : bool  = False     # Supersampling aktivieren/deaktivieren
+    supersampling_enabled   : bool  = True     # Supersampling aktivieren/deaktivieren
     supersampling_factor    : int   = 2         # Faktor für Supersampling (z.B. 2 = 4x Supersampling, 3 = 9x Supersampling, etc.)
 
     # Postprocessing
